70d39ac6e04fc40baab703005ab83b86c0f441dd
andreacagnina <amirkal@libero.it>
2024-06-25 15:55:37 +0200

    [feat] removed console.log

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 let km = prompt(`Inserire il numero di chilometri che si vogliono percorrere`);
-console.log(km)
+
 let age = prompt(`Inserire l'età del passeggero`);
-console.log(age)
+
 
 const euroKm = 0.21;
-console.log(euroKm)
+
 
 let euroTicket = euroKm * km;
-console.log(euroTicket)
+
 
 let discount = 0;
 
@@ -19,12 +19,12 @@ if (age < 18) {
 else if (age > 65) {
     discount = 40;
 }
-console.log(discount)
+
 
 let discountValue = euroTicket * discount / 100;
 
 
 let discountedTicket = euroTicket - discountValue
-console.log(discountedTicket)
+
 
 document.getElementById('ticketCost').innerHTML = discountedTicket.toFixed(2) + '&euro;' + ' ' + '<em> Grazie per il tuo acquisto!</em>';
