205518c37fd4bb5e47d911b552010332d1c1ad3a
andreacagnina <amirkal@libero.it>
2024-06-25 14:52:44 +0200

    [FEAT] DICHIARATA VAR AGE E CONST EUROKM

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 let km = prompt(`Inserire il numero di chilometri che si vogliono percorrere`);
 console.log(km)
+let age = prompt(`Inserire l'età del passeggero`);
+console.log(age)
+
+const euroKm = 0.21;
+console.log(euroKm)
