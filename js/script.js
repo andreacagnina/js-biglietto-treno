86b2b6ae8a8e116e7b361179b698bd134a0dc188
andreacagnina <amirkal@libero.it>
2024-06-25 15:34:26 +0200

    [FEAT] dichiarata variabile prezzo biglietto scontato

--- js/script.js
+++ js/script.js
@@ -8,3 +8,23 @@ console.log(euroKm)
 
 let euroTicket = euroKm * km;
 console.log(euroTicket)
+
+let discount = 0;
+
+
+if (age < 18) {
+    discount = 20;
+}
+
+else if (age > 65) {
+    discount = 40;
+}
+console.log(discount)
+
+let discountValue = euroTicket * discount / 100;
+
+
+let discountedTicket = euroTicket - discountValue
+console.log(discountedTicket)
+
+document.getElementById('ticketCost').innerHTML = ' '
